3feb5fa644c0f5466539a667796c3af0add1bc03
megumi <1696597764@qq.com>
2023-08-02 21:22:51 +0800

    全局统一异常处理

---
+++ common/service-base/src/main/java/com/servicebase/handler/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.servicebase.handler;
+
+import com.commonutils.R;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public R error(Exception e){
+        e.printStackTrace();
+        return R.error();
+    }
+}
