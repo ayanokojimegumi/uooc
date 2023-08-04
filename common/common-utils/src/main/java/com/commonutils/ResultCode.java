a1bd2bfcd8b5a2c61d7853c67ab84e010947824f
megumi <1696597764@qq.com>
2023-08-04 17:22:28 +0800

    返回结果封装类

--- common/common-utils/src/main/java/com/commonutils/ResultCode.java
+++ common/common-utils/src/main/java/com/commonutils/ResultCode.java
@@ -3,8 +3,8 @@ package com.commonutils;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public class ResultCode {
-    @Schema(title = "1表示成功")
-    public static Integer SUCCESS = 0;
-    @Schema(title = "0表示成功")
-    public static Integer ERROR = 1;
+    @Schema(title = "20000表示成功")
+    public static Integer SUCCESS = 20000;
+    @Schema(title = "20001表示失败")
+    public static Integer ERROR = 20001;
 }
