faf19e8e40906ff3bec19519d6c28b3ec4a7a517
megumi <1696597764@qq.com>
2023-08-04 17:23:18 +0800

    全局异常处理器

--- common/service-base/src/main/java/com/servicebase/handler/GlobalExceptionHandler.java
+++ common/service-base/src/main/java/com/servicebase/handler/GlobalExceptionHandler.java
@@ -1,15 +1,17 @@
 package com.servicebase.handler;
 
 import com.commonutils.R;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public R error(Exception e){
+        log.error(e.getMessage());
         e.printStackTrace();
         return R.error();
     }
